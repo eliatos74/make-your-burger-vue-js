3a2a23e792c0e952707ed685e2d760c217565d8f
eliatos74 <eliassantos95@hotmail.com>
2024-05-16 16:47:36 -0300

    adiconando json no back

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -2,7 +2,7 @@
   <div>
     <p>Componente de Mensagem</p>
     <div>
-      <form id="burger-form">
+      <form id="burger-form" @submit="createBurger">
         <div class="input-container">
           <label for="nome">Nome do Cliente:</label>
           <input
@@ -69,7 +69,6 @@ export default {
       pao: null,
       carne: null,
       opcionais: [],
-      status: "solicitado",
       msg: null,
     };
   },
@@ -83,6 +82,32 @@ export default {
       this.paes = data.paes;
       this.carnes = data.carnes;
       this.opcionaisdata = data.opcionais;
+    },
+    async createBurger(e) {
+      e.preventDefault();
+      const data = {
+        nome: this.nome,
+        carne: this.carne,
+        pao: this.pao,
+        opcionais: Array.from(this.opcionais),
+        status: "Solicitado",
+      };
+
+      const dataJson = JSON.stringify(data);
+
+      const req = await fetch("http://localhost:3000/burgers", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson,
+      });
+
+      const res = await req.json();
+
+      // colocar uma msg de sistema
+
+      // limpar msg
+
+      // limpar os campos
     },
   },
   mounted() {
@@ -158,4 +183,3 @@ select {
   color: #222;
 }
 </style>
-
