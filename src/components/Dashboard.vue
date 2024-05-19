210e3960fd3059ac25b633cc27681fba4277b5ab
eliatos74 <eliassantos95@hotmail.com>
2024-05-19 16:00:08 -0300

    Resgatando pedidos do banco

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,33 +11,16 @@
       </div>
     </div>
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão de trigo</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burges" :key="burger.id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
         <div>
           <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
-          </ul>
-        </div>
-        <div>
-          <select name="status" id="status">
-            <option value="">Selecione</option>
-          </select>
-          <button class="delete-btn">Cancelar</button>
-        </div>
-      </div>
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão de trigo</div>
-        <div>Maminha</div>
-        <div>
-          <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">
+              {{ opcional }}
+            </li>
           </ul>
         </div>
         <div>
@@ -54,6 +37,30 @@
 <script>
 export default {
   name: "Dashboard",
+  data() {
+    return {
+      burges: null,
+      burger_id: null,
+      status: [],
+    };
+  },
+
+  methods: {
+    async getPedidos() {
+      const req = await fetch("http://localhost:3000/burgers");
+
+      const data = await req.json();
+
+      this.burges = data;
+
+      console.log(this.burges);
+
+      // resgatar os status
+    },
+  },
+  mounted() {
+    this.getPedidos();
+  },
 };
 </script>
 
@@ -108,10 +115,8 @@ select {
   cursor: pointer;
   transition: 0.5s;
 }
-.delete-btn:hover{
-    background-color: transparent;
-    color: #222;
-
+.delete-btn:hover {
+  background-color: transparent;
+  color: #222;
 }
-
 </style>
