0a17d2c3bf0b36b0b51be96ab84d1b43ed828f29
eliatos74 <eliassantos95@hotmail.com>
2024-05-19 17:06:36 -0300

    Removendo pedidos do sistema

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -26,11 +26,18 @@
         <div>
           <select name="status" id="status">
             <option value="">Selecione</option>
-            <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">
+            <option
+              v-for="s in status"
+              :key="s.id"
+              value="s.tipo"
+              :selected="burger.status == s.tipo"
+            >
               {{ s.tipo }}
             </option>
           </select>
-          <button class="delete-btn">Cancelar</button>
+          <button class="delete-btn" @click="deleteBurger(burger.id)">
+            Cancelar
+          </button>
         </div>
       </div>
     </div>
@@ -68,6 +75,17 @@ export default {
       const data = await req.json();
 
       this.status = data;
+    },
+    async deleteBurger(id) {
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "DELETE",
+      });
+
+      const res = await req.json();
+
+      // mensagem de pedido deletado do sistema
+
+      this.getPedidos();
     },
   },
   mounted() {
